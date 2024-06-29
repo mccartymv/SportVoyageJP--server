659ebe34b166a03df6b967c967168c50735aec40
mccartymv <mvmccart@gmail.com>
2024-06-28 20:20:08 -0400

    add new route for venues

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // Routes
 router.get('/games', getAllGames);
 router.get('/games/team/:teamName', getGamesByTeam);
+router.get('/games/venue/:venueName', getGamesByVenue);
 router.get('/teams', getUniqueTeams);
 router.get('/venues', getUniqueVenues);
 
