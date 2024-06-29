2cd7d5f01abed5aff737c1633848468899a21d41
mccartymv <mvmccart@gmail.com>
2024-06-28 20:05:26 -0400

    Add routes to get unique team names and venues with their counts.

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { getAllGames, getGamesByTeam } = require('../controllers/gameController');
+const { getAllGames, getGamesByTeam, getUniqueTeams, getUniqueVenues } = require('../controllers/gameController');
+
 const router = express.Router();
 
 // Routes
 router.get('/games', getAllGames);
 router.get('/games/team/:teamName', getGamesByTeam);
+router.get('/teams', getUniqueTeams);
+router.get('/venues', getUniqueVenues);
 
 module.exports = router;
